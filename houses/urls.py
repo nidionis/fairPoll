b9fa3b13f8a34e2e7340588292f12ff65149209b
Your Name <you@example.com>
2026-04-17 21:26:30 +0200

    houses/urls.py

--- houses/urls.py
+++ houses/urls.py
@@ -5,7 +5,7 @@ app_name = 'houses'
 
 urlpatterns = [
     path('', views.house_list, name='house_list'),
-    path('user-autocomplete/', views.UserAutocomplete.as_view(), name='user-autocomplete'),
+    #path('user-autocomplete/', views.UserAutocomplete.as_view(), name='user-autocomplete'),
     path('create/', views.house_create, name='house_create'),
     path('<int:pk>/', views.house_detail, name='house_detail'),
     path('<int:pk>/integrate/', views.create_integration_poll, name='create_integration_poll'),
